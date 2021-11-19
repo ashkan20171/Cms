cfeb3d574230c5b70b5426ebf05815af0af4508a
ashkan20171 <ashkan.mataee@yahoo.com>
2021-11-19 20:59:14 +0330

    add files , edit index

--- index.php
+++ index.php
@@ -2,8 +2,14 @@
 <head>
     <meta charset="UTF-8">
     <title>weblog</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h1>hello</h1>
+    <div class="alert alert-primary">
+        سلام
+    </div>
 </body>
+     <script src="js/bootstrap.min.js"></script>
+     <script src="js/jquery-3.6.0.min.js"></script>
 </html>
